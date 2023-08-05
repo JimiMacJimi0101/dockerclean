71a4ffda06f49f9dd69222040f0fb9dcd3740d25
JimiMacJimi0101 <102225605+JimiMacJimi0101@users.noreply.github.com>
2023-08-05 11:48:36 +0200

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,34 +1,17 @@
-import subprocess
-import sys
+import time
+import docker
 
-def stop_and_remove_containers(docker_host, container_names_to_keep):
-    # Get the list of all container IDs
-    container_ids = subprocess.check_output(['docker', '-H', docker_host, 'ps', '-aq'], encoding='ascii')
-    container_ids = container_ids.strip().split()
+client = docker.DockerClient(base_url='tcp://118.244.195.128:2375')
 
-    # Filter out container IDs based on their names
-    container_ids_to_remove = []
-    for container_id in container_ids:
-        container_name = subprocess.check_output(['docker', '-H', docker_host, 'inspect', '-f', '{{.Name}}', container_id], encoding='ascii').strip()
-        if container_name not in container_names_to_keep:
-            container_ids_to_remove.append(container_id)
-
-    # Stop and remove containers
-    if container_ids_to_remove:
-        for container_id in container_ids_to_remove:
-            # Stop the container
-            subprocess.check_call(['docker', '-H', docker_host, 'stop', container_id])
-            # Get the name of the container
-            container_name = subprocess.check_output(['docker', '-H', docker_host, 'inspect', '-f', '{{.Name}}', container_id], encoding='ascii').strip()
-            # Remove the container
-            subprocess.check_call(['docker', '-H', docker_host, 'rm', container_id])
-            # Print the name of the removed container
-            print(f'Removed container: {container_name}')
-
-if __name__ == '__main__':
-    # Parse command line arguments
-    docker_host = sys.argv[1]
-    container_names_to_keep = sys.argv[2:]
-
-    # Stop and remove containers
-    stop_and_remove_containers(docker_host, container_names_to_keep)
+while True:
+    containers = client.containers.list(all=True)
+    for container in containers:
+        if "mts" not in container.name:
+            try:
+                container.stop()
+                container.remove()
+                print(f"Stopped and removed container: {container.name}")
+            except docker.errors.APIError as e:
+                print(f"Error stopping/removing container {container.name}: {e}")
+    
+    time.sleep(5)  # Pause for 5 seconds before the next iteration
